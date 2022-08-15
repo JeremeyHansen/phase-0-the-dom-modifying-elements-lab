37c5a48609a8800e60ecd075b72c2de46a6ac23d
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-15 09:50:07 -0400

    commit tree 9221b15c6ac81687244b928b50789fcecc0c3b8b

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement('h1')
+
+// document.body.append(newHeader);
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
